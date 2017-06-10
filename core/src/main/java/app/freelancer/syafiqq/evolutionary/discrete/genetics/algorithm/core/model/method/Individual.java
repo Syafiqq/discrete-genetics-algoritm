f66f29c6540064e2e4bcc330267c84136f7ddc60
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-10 17:57:03 +0700

    Add individual data class

---
+++ core/src/main/java/app/freelancer/syafiqq/evolutionary/discrete/genetics/algorithm/core/model/method/Individual.java
@@ -0,0 +1,12 @@
+package app.freelancer.syafiqq.evolutionary.discrete.genetics.algorithm.core.model.method;
+
+/*
+ * This <discrete-genetics-algoritm> created by : 
+ * Name         : syafiq
+ * Date / Time  : 07 June 2017, 2:58 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class Individual
+{
+}
