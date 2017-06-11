216abac8149600b2f612d2bdcde2205e5010489e
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-11 08:44:19 +0700

    Add recorded penalty value and supply

--- case/src/main/java/app/freelancer/syafiqq/evolutionary/discrete/genetics/algorithm/cases/a/model/method/IndividualImpl.java
+++ case/src/main/java/app/freelancer/syafiqq/evolutionary/discrete/genetics/algorithm/cases/a/model/method/IndividualImpl.java
@@ -1,5 +1,6 @@
 package app.freelancer.syafiqq.evolutionary.discrete.genetics.algorithm.cases.a.model.method;
 
+import app.freelancer.syafiqq.evolutionary.discrete.genetics.algorithm.cases.a.model.method.utils.DoubleMap;
 import app.freelancer.syafiqq.evolutionary.discrete.genetics.algorithm.core.model.method.Individual;
 import java.util.Arrays;
 import java.util.Objects;
@@ -12,20 +13,33 @@ import org.jetbrains.annotations.NotNull;
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-@SuppressWarnings({"WeakerAccess", "unused"}) public class IndividualImpl extends Individual
+public class IndividualImpl extends Individual
 {
     private Integer[] fertilizers;
     private Integer[] processedFertilizer;
+    private DoubleMap supply;
     private double cost;
     private double fitness;
+    private double penalty;
 
     public IndividualImpl()
     {
+        this.supply = new DoubleMap();
+        this.generateElement();
     }
 
     public IndividualImpl(@NotNull Integer[] fertilizers)
     {
         this.fertilizers = fertilizers;
+        this.supply = new DoubleMap();
+        this.generateElement();
+    }
+
+    private void generateElement()
+    {
+        this.supply.put("nitrogen", 0.0);
+        this.supply.put("phosphorus", 0.0);
+        this.supply.put("potassium", 0.0);
     }
 
     public Integer[] getFertilizers()
@@ -68,6 +82,51 @@ import org.jetbrains.annotations.NotNull;
         this.processedFertilizer = processedFertilizer;
     }
 
+    public DoubleMap getElements()
+    {
+        return this.supply;
+    }
+
+    public void setElements(DoubleMap supply)
+    {
+        this.supply = supply;
+    }
+
+    public void plus(String k, Double v)
+    {
+        getElements().plus(k, v);
+    }
+
+    public void minus(String k, Double v)
+    {
+        getElements().minus(k, v);
+    }
+
+    public void multiply(String k, Double v)
+    {
+        getElements().multiply(k, v);
+    }
+
+    public void divide(String k, Double v)
+    {
+        getElements().divide(k, v);
+    }
+
+    public Double get(String k)
+    {
+        return supply.get(k);
+    }
+
+    public Double put(String key, Double value)
+    {
+        return supply.put(key, value);
+    }
+
+    public void syncProcessedFertilizer(int individualWindow)
+    {
+        System.arraycopy(this.fertilizers, 0, this.processedFertilizer, 0, individualWindow);
+    }
+
     @Override public boolean equals(Object o)
     {
         if(this == o)
@@ -81,26 +140,27 @@ import org.jetbrains.annotations.NotNull;
         IndividualImpl that = (IndividualImpl) o;
         return Double.compare(that.getCost(), getCost()) == 0 &&
                 Double.compare(that.getFitness(), getFitness()) == 0 &&
+                Double.compare(that.penalty, penalty) == 0 &&
                 Arrays.equals(getFertilizers(), that.getFertilizers()) &&
-                Arrays.equals(getProcessedFertilizer(), that.getProcessedFertilizer());
+                Arrays.equals(getProcessedFertilizer(), that.getProcessedFertilizer()) &&
+                Objects.equals(supply, that.supply);
     }
 
     @Override public int hashCode()
     {
-        return Objects.hash(getFertilizers(), getCost(), getFitness(), getProcessedFertilizer());
+        return Objects.hash(getFertilizers(), getProcessedFertilizer(), getCost(), getFitness(), penalty, supply);
     }
 
     @Override public String toString()
     {
-        return "IndividualImpl{" + "fertilizers=" + Arrays.toString(fertilizers) +
-                ", cost=" + cost +
-                ", fitness=" + fitness +
-                ", processedFertilizer=" + Arrays.toString(processedFertilizer) +
-                '}';
-    }
-
-    public void syncProcessedFertilizer(int individualWindow)
-    {
-        System.arraycopy(this.fertilizers, 0, this.processedFertilizer, 0, individualWindow);
+        final StringBuilder sb = new StringBuilder("IndividualImpl{");
+        sb.append("fertilizers=").append(Arrays.toString(fertilizers));
+        sb.append(", processedFertilizer=").append(Arrays.toString(processedFertilizer));
+        sb.append(", cost=").append(cost);
+        sb.append(", fitness=").append(fitness);
+        sb.append(", penalty=").append(penalty);
+        sb.append(", supply=").append(supply);
+        sb.append('}');
+        return sb.toString();
     }
 }
